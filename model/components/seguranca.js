feceb2dc873eded7cc96a3156de7e509997e6fbe
Gabrielh7Amorim <gabriel.amorim12@fatec.sp.gov.br>
2022-03-23 11:32:51 -0300

    config hash

--- model/components/seguranca.js
+++ model/components/seguranca.js
@@ -2,9 +2,12 @@
 function ocultarSenha(senha){
     var sha1 = require('sha1');
     console.log(senha);
-    var hash = sha1(senha);
-    console.log(hash);
+    var hash = sha1(senha); //chamada de função da biblioteca sha1
+    console.log(hash); //exibe o que foi gerado da função sha1
     return hash;
 }
+
+//sha1 é o mais indicado para fazer criptografias
+//hash - tudo o que for alterado será feito uma tradução
 
 module.exports = {ocultarSenha}
