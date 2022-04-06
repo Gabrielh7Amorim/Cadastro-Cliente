0e08367ce60690b5a44b5d39c2f61d18ec2f2997
Gabrielh7Amorim <gabriel.amorim12@fatec.sp.gov.br>
2022-04-06 11:39:00 -0300

    teste

--- model/repositories/index.js
+++ model/repositories/index.js
@@ -1,7 +1,7 @@
 (async=>{
     const db = require("./db");
     console.log('SELECT * FROM USUARIO');
-    const usuarios = await db.selectUsuario();
+    const usuarios =  db.selectUsuario();
     console.log(usuarios);
 })();
 // função main assíncrona
