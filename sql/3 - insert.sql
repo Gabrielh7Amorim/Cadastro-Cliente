5c8618d1ea4f0b4035892214f0d0a85cffcb300d
Gabrielh7Amorim <gabriel.amorim12@fatec.sp.gov.br>
2022-04-06 09:47:09 -0300

    dependência mysql  e preparação para autenticação

---
+++ sql/3 - insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (nome, senha) VALUES ('Icaro', 'dihsiwi87752hds9jslskh');
+INSERT INTO usuario (nome, senha) VALUES ('Aline', 'hdsaiididsak99h7t73y73');
---
+++ sql/5 - update.sql
@@ -0,0 +1,2 @@
+UPDATE usuario SET nome='Frank' WHERE id=1;
+UPDATE usuario SET nome='Icaro' WHERE id=2;
